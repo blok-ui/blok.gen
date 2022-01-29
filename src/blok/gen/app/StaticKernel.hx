8988c0ce429b30950ebf4f769393417b8c036402
wartman <faxmaster@gmail.com>
2022-01-29 08:51:00 -0600

    Added some hooks for generation

--- src/blok/gen/app/StaticKernel.hx
+++ src/blok/gen/app/StaticKernel.hx
@@ -5,9 +5,11 @@ package blok.gen.app;
 #end
 
 import blok.context.Context;
+import blok.state.Observable;
 import blok.foundation.routing.history.StaticHistory;
 import blok.gen.routing.HistoryService;
 import blok.gen.core.Kernel;
+import blok.gen.core.Config;
 import blok.gen.build.Visitor;
 import blok.gen.build.FileWriter;
 import blok.gen.build.MetadataService;
@@ -16,19 +18,27 @@ import blok.gen.cli.NodeConsole;
 
 using tink.CoreApi;
 
+enum StaticKernelState {
+  Pending;
+  Generating;
+  Complete(config:Config);
+  Failed(err:Error);
+}
+
 class StaticKernel extends Kernel {
+  public final hooks:Observable<StaticKernelState> = new Observable(Pending);
+
   function addCoreServices(context:Context) {
     context.addService(new HistoryService(new StaticHistory('/')));
     context.addService(new MetadataService());
   }
 
   public function run() {
-    // hooks.status.update(Generating);
-
     var display = new Display(new NodeConsole());
     var visitor = new Visitor(this, display);
     var writer = new FileWriter(config.ssr.destination);
-
+    
+    hooks.update(Generating);
     display.write('Building [ ${config.site.title} ]');
     display.work();
 
@@ -39,11 +49,12 @@ class StaticKernel extends Kernel {
           writer.write(result.path, result.contents);
         })).handle(o -> switch o {
           case Success(_):
-            // hooks.status.update(Complete(config));
+            hooks.update(Complete(config));
             display.success('Build completed with no errors.');
             Sys.exit(0);
           case Failure(failure):
             display.error('Build failed with: ${failure.message}');
+            hooks.update(Failed(failure));
             Sys.exit(1);
         });
       case Failure(failure):
